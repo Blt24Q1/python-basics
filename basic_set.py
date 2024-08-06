4a1fbaeafbbc9bc918e45757db48542992445fd2
Blt24Q1 <blt24q1@gmail.com>
2024-08-06 11:09:41 +0900

    set 집합 연산, 메서드

--- basic_set.py
+++ basic_set.py
@@ -1,6 +1,6 @@
 evens = { 0, 2, 4, 6, 8, 10 }   # 짝수 집합
 odds = { 1, 3, 5, 7, 9 }    # 홀수 집합
-numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} # 전체 집합
+numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 } # 전체 집합
 mthree = { 0, 3, 6, 9 }     # 3의 배수 집합
 
 
@@ -37,8 +37,58 @@ def set_methods():
     """
     set의 메서드들
     """
+    print("numbers:", numbers, len(numbers))
+    numbers.add(10)
+    print("numbers:", numbers, len(numbers))
+    numbers.add(10) # 중복을 허용하지 않는 자료형
+    print("numbers:", numbers, len(numbers))
+
+    print("evens:", evens)
+    evens.add(3)    # 추가
+    print("evens:", evens)
+    evens.discard(3) # 3 삭제
+    print("evens:", evens)
+    evens.discard(3) # 삭제
+    # discard는 없어도 오류 없이 무시
+    if 3 in evens:
+        evens.remove(3) # 삭제할 데이터 없으면 KeyError
+
+    evens.update({10})
+    print("evens:", evens)
+
+
+def set_oper():
+    """
+    set의 집합 연산
+    """
+    numbers.add(10)
+
+    # 합집합 : |, union
+    print(evens | odds == numbers) # 짝수 집합 합집합 홀수 집합 == 전체 집합
+    print(evens.union(odds) == numbers)
+
+    # 교집합 : &, intersection
+    print(evens & mthree == { 0, 6 })
+    print(evens.intersection(mthree) == { 0, 6 })
+
+    # 차집합 : -, difference
+    print(numbers - odds == evens) # 전체 집합 차집합 홀수 집합 == 짝수 집합
+    print(numbers.difference(odds) == evens)
+
+    # 판별 함수
+    print(numbers.issuperset(evens), numbers.issuperset(odds)) # 부모 집합 여부 판별
+    print(evens.issubset(numbers), odds.issubset(numbers))  # 부분 집합 여부 판별
+
+
+def loop():
+    for num in numbers:
+        print(num, end="\t")
+    else:
+        print()
 
 
 if __name__ == "__main__":
     # define_set()
-    set_methods()
+    # set_methods()
+    # set_oper()
+    loop()
